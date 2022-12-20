02056d38130e34a130e7a3d95c7ea9141e46f9a9
៚ᴍʀͥ.ᴀͣᴍͫɪʏᴀ༒ᴏꜰᴄ͢⁸⁵⁴¹༆࿐ <techwithamee@gmail.com>
2022-12-20 19:39:55 +0530

    Add files via upload

--- commands/search.js
+++ commands/search.js
@@ -158,7 +158,7 @@ cmd({
     )
     //---------------------------------------------------------------------------
 cmd({
-            pattern: "image",
+            pattern: "img",
             category: "search",
             desc: "Searches Image on Google",
             use: '<text>',
